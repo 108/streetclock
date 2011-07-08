47b6602b42147876625b6da8a11444d9d26a5366
108media <info@108media.hu>
2011-07-08 18:38:58 +0200

    New title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Street Clock  by Free CSS Templates</title>
+<title>Git Test</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
